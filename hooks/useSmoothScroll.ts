44b178c005c0edaeca43582ef8ba49f437d3a922
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2026-03-04 17:04:49 +0600

    fix: scroll smoothness increase for desktop

--- hooks/useSmoothScroll.ts
+++ hooks/useSmoothScroll.ts
@@ -16,11 +16,11 @@ export function useSmoothScroll() {
 
     if (!lenisInstance && !isMobile) {
       lenisInstance = new Lenis({
-        duration: 1.2,
+        duration: 2,
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        lerp: 0.1,
+        lerp: 0.3,
         infinite: false,
-        syncTouch: false,
+        syncTouch: true,
       });
 
       function raf(time: number) {
